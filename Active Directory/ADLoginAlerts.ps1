5a8978c410d5b5f0bd115c19eaf474d854f58806
Jannik <93577959+jannikk09@users.noreply.github.com>
2024-01-25 13:06:42 +0100

    Update ADLoginAlerts.ps1

--- Active Directory/ADLoginAlerts.ps1
+++ Active Directory/ADLoginAlerts.ps1
@@ -1,55 +1,57 @@
-# Parameters for Email Sending
-$smtpServer = "smtp.example.com"
-$smtpFrom = "alert@example.com"
-$smtpTo = "admin@example.com"
-$smtpSubject = "Unusual Login Activity Detected"
-$smtpBody = "Unusual login activity detected in Active Directory. Details: `r`n"
+# Check current time
+$currentTime = Get-Date
+$startTimeParam = "18:00" # Start of outside working hours
+$endTimeParam = "06:00"   # End of outside working hours
 
-# Parameters for Login Time Verification
-$startTime = "18:00" # Start of Outside Working Hours
-$endTime = "06:00"   # End of Outside Working Hours
+$start = [DateTime]::ParseExact($startTimeParam, "HH:mm", $null)
+$end = [DateTime]::ParseExact($endTimeParam, "HH:mm", $null)
 
-function IsOutsideWorkingHours($logonTime) {
-    $start = [DateTime]::ParseExact($startTime, "HH:mm", $null)
-    $end = [DateTime]::ParseExact($endTime, "HH:mm", $null)
-    $logon = [DateTime]::ParseExact($logonTime, "HH:mm:ss", $null)
+if ($currentTime.TimeOfDay -lt $end.TimeOfDay -or $currentTime.TimeOfDay -gt $start.TimeOfDay) {
+    # Parameters for Email Sending
+    $smtpServer = "smtp.example.com"
+    $smtpFrom = "alert@example.com"
+    $smtpTo = "admin@example.com"
+    $smtpSubject = "Unusual Login Activity Detected"
+    $smtpBody = "Unusual login activity detected in Active Directory. Details: `r`n"
 
-    if ($start -lt $end) {
-        return ($logon -lt $start) -or ($logon -gt $end)
-    } else {
-        return ($logon -gt $end) -and ($logon -lt $start)
+    function IsOutsideWorkingHours($logonTime) {
+        $logon = [DateTime]::ParseExact($logonTime, "HH:mm:ss", $null)
+
+        if ($start -lt $end) {
+            return ($logon -lt $start) -or ($logon -gt $end)
+        } else {
+            return ($logon -gt $end) -and ($logon -lt $start)
+        }
     }
-}
 
-# Retrieving Security Logs
-$query = @{
-    LogName = 'Security'
-    ID = 4624
-    MaxEvents = 50
-}
-$events = Get-WinEvent -FilterHashtable $query
+    # Retrieving Security Logs
+    $events = Get-EventLog -LogName Security -InstanceId 4624 -Newest 50
 
-# Checking the Events
-foreach ($event in $events) {
-    $logonTime = $event.TimeGenerated.ToString("HH:mm:ss")
-    
-    if (IsOutsideWorkingHours $logonTime) {
-        $smtpBody += "Date/Time: " + $event.TimeGenerated + ", User: " + $event.ReplacementStrings[5] + "`r`n"
+    # Checking the Events
+    foreach ($event in $events) {
+        $username = $event.ReplacementStrings[5]
+        $logonTime = $event.TimeGenerated.ToString("HH:mm:ss")
+        
+        if (IsOutsideWorkingHours $logonTime) {
+            $smtpBody += "Date/Time: " + $event.TimeGenerated + ", User: " + $username + "`r`n"
+        }
     }
-}
 
-# Sending an Email if Unusual Activities are Found
-if ($smtpBody -ne "Unusual login activity detected in Active Directory. Details: `r`n") {
-    # Erstellen eines MailMessage-Objekts
-    $mailMessage = New-Object System.Net.Mail.MailMessage
-    $mailMessage.From = [System.Net.Mail.MailAddress]::new($smtpFrom)
-    $mailMessage.To.Add($smtpTo)
-    $mailMessage.Subject = $smtpSubject
-    $mailMessage.Body = $smtpBody
-    $mailMessage.IsBodyHtml = $false # Set this to $true if you are using HTML
-    $mailMessage.BodyEncoding = [System.Text.Encoding]::UTF8
+    # Sending the Email
+    if ($smtpBody -ne "Unusual login activity detected in Active Directory. Details: `r`n") {
+        # Creating a MailMessage object
+        $mailMessage = New-Object System.Net.Mail.MailMessage
+        $mailMessage.From = [System.Net.Mail.MailAddress]::new($smtpFrom)
+        $mailMessage.To.Add($smtpTo)
+        $mailMessage.Subject = $smtpSubject
+        $mailMessage.Body = $smtpBody
+        $mailMessage.IsBodyHtml = $false # Set to $true for HTML
+        $mailMessage.BodyEncoding = [System.Text.Encoding]::UTF8
 
-    # Erstellen und Konfigurieren des SmtpClient-Objekts
-    $smtpClient = New-Object System.Net.Mail.SmtpClient($smtpServer)
-    $smtpClient.Send($mailMessage)
+        # Creating and configuring the SmtpClient object
+        $smtpClient = New-Object System.Net.Mail.SmtpClient($smtpServer)
+        $smtpClient.Send($mailMessage)
+    }
+} else {
+    Write-Host "Current time is outside the set time window. Script is not being executed."
 }
