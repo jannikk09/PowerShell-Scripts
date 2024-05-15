12ecbd5a836e1baf11772cce29d904116760890f
Jannik <93577959+jannikk09@users.noreply.github.com>
2024-05-15 11:39:39 +0200

    Update BR-DeleteTemp.ps1

--- Intune/Remediation-Scripts/Delete Temp/BR-DeleteTemp.ps1
+++ Intune/Remediation-Scripts/Delete Temp/BR-DeleteTemp.ps1
@@ -1,2 +1,7 @@
-Get-ChildItem -Path "C:\Windows\Temp" -Recurse | Remove-Item -Force -Recurse
-Get-ChildItem -Path "$env:TEMP" -Recurse | Remove-Item -Force -Recurse
+try {
+    Get-ChildItem -Path "C:\Windows\Temp", "$env:TEMP" -Recurse | Remove-Item -Force -Recurse -ErrorAction Stop
+    exit 0  # Erfolgreich
+} catch {
+    Write-Error "Fehler beim Löschen der Dateien: $_"
+    exit 1  # Fehler
+}
